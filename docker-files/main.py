a9f5b2d2091df68a82dc742fbbdc8393302dc193
MethaneRain <justin.richling@gmail.com>
2020-07-20 18:54:57 +0000

    Update main.py
    add print statement to instruct on how to run the jupyter notebook

--- docker-files/main.py
+++ docker-files/main.py
@@ -1,2 +1,6 @@
+print("\n_-_-_-_-_-_-_-_-_-_-_-_\n")
 print("Docker container works!")
-print("_-_-_-_-_-_-_-_-_-_-_-_\n")
+
+print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+print("run: $docker run -it --rm -p 8888:8888 methanerain/dynamics:latest bash")
+print("then run: $jupyter notebook --ip=0.0.0.0 --no-browser --allow-root")
