a8e30dec03731f40720039abf56352240bf74edd
MethaneRain <justin.richling@gmail.com>
2020-07-21 00:59:56 +0000

    Create readme.py

---
+++ docker-files/readme.py
@@ -0,0 +1,7 @@
+print("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+print("To run the jupyter notebook you must run the docker image like:")
+print("run: $docker run -it --rm -p 8888:8888 methanerain/dynamics:latest bash")
+
+print("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+print("One you are in the bash/bin command line in the docker container:")
+print("then run: #jupyter notebook --ip=0.0.0.0 --no-browser --allow-root\n")
