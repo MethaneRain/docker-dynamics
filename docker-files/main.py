e63edfe99a550e3e20a931eca4ab629f3164c762
MethaneRain <justin.richling@gmail.com>
2020-07-21 01:15:13 +0000

    Update main.py

--- docker-files/main.py
+++ docker-files/main.py
@@ -2,5 +2,6 @@ print("\n_-_-_-_-_-_-_-_-_-_-_-_\n")
 print("Docker container works!")
 
 print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-print("run: $docker run -it --rm -p 8888:8888 methanerain/dynamics:latest bash")
-print("then run: $jupyter notebook --ip=0.0.0.0 --no-browser --allow-root")
+print("run: $docker run -it --rm -p 8888:8888 methanerain/dynamics:latest /bin/bash")
+print("Then run the readme.py script with:")
+print("#python3 readme.py")
